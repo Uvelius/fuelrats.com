7a8854a65b9792ac572e0651ce36ef6e6ca0bc16
Uvelius <35564469+Uvelius@users.noreply.github.com>
2018-01-18 17:02:27 +0100

    left nav panel

--- components/Nav.js
+++ components/Nav.js
@@ -5,19 +5,22 @@ import React from 'react'
 
 
 
-
 // Constants
 const links = {
-  Blog: {
-    href: '/blog/all',
-    as: '/blog',
-  },
-  Statistics: {
-    href: '/statistics',
-  },
-  Leaderboard: {
-    href: '/leaderboard',
-  },
+Blog: {
+href: '/blog/all',
+as: '/blog',
+},
+Statistics: {
+href: '/statistics',
+},
+Leaderboard: {
+href: '/leaderboard',
+},
+'Stories, Art, & Toons:' {
+href: '/blog/category/138',
+},
+
 }
 
 
